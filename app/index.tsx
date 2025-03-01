640181af8b9e6efb8676ebd7eaa1bcc1fe1b70f2
Cody Nguyen <Thanh.ng13@hotmail.com>
2025-03-01 07:54:37 -0700

    created a button to route to assignment 3

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+    const router = useRouter();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to Assignment 3!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => router.push('/assignment3')}
+        >
+          <Text style={styles.buttonText}>Go to Assignment 3</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +25,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#40E0D0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    padding: 9,
+    height: 60,
+    width: 120,
+    marginTop: 20,
+    borderRadius: 20,
+  },
+  buttonText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
